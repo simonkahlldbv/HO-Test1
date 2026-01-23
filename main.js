d4ddc6b5e0cbfca4d6fdd1103e68e32968c3abd4
simonkahlldbv <simonkahl.ldbv@gmail.com>
2026-01-23 10:47:50 +0100

    sa

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let rightModel = null;
 let isCompareMode = false;
 let syncingCameras = false;
 
-// Kamera-Startpositionen (noch höher und weiter weg für beste Übersicht)
-const DEFAULT_CAMERA_POSITION = { x: 600, y: 1000, z: 600 };
+// Kamera-Startpositionen (mittigere Ansicht von oben)
+const DEFAULT_CAMERA_POSITION = { x: 0, y: 1000, z: 800 };
 const DEFAULT_CAMERA_TARGET = { x: 0, y: 0, z: 0 };
 
 const years = ['1150', '1175', '1374', '1550', '1630', '1936'];
